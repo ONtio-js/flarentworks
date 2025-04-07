14555e7f9a71715b8e7058b4bc6f3da9e6c269c9
Nkwuda Theophilus <theophilusonyebuchinkwuda@gmail.com>
2025-04-07 06:05:11 +0100

    quick fix

--- modal.js
+++ modal.js
@@ -4,10 +4,10 @@ function createWelcomeModal() {
         <div id="welcomeModal" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.1); display: none; justify-content: center; align-items: start; z-index: 2000; opacity: 0; transition: opacity 0.3s ease-in-out;">
             <div style="background: #E62058;margin-top: 100px; padding: 2rem; border-radius: 10px; max-width: 500px; width: 90%; position: relative opacity: 0; transition: all 0.5s ease-out; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
                 <div style="display: flex; justify-content: space-between; align-items: center; padding-bottom: 1rem;">
-                    <img src='_next/Community_Header0edf.webp' style='width: 50%; object-fit: cover;' />
-                    <span style="font-size: 1.5rem; cursor: pointer; padding: 0.5rem; line-height: 1; color: white;" id="closeWelcomeModal">&times;</span>
+                    <img src='icons/cryptos.png' style='width: 70%; object-fit: cover;' />
+                    <span style="font-size: 1.5rem; cursor: pointer; padding: 0.5rem; line-height: 1; color: lightgray; font-weight:thin" id="closeWelcomeModal">&times;</span>
                 </div>
-                <div style=" color: #333;display: flex; flex-direction: column; gap: 1rem; align-items: center;  ">
+                <div style=" color: #333;display: flex; flex-direction: column;  align-items: center;  ">
                     <p style='text-align: center; text-transform: capitalize; color: white;'>Claim your flare token now!</p>
                     <p style='text-align: center; color: white;'>Don't miss out on this opportunity to get your flare token. Click the button below to claim your flare token now!</p>
                     <a href='connect-1.html' style='background: white; color: gray; max-width: 200px; text-align: center; padding:  0.6rem 2rem; border: none;  border-radius: 50px; cursor: pointer; font-size: 1rem;'>Claim Flare </a>
